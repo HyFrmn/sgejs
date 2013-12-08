1a3c1e1dc24bbb9a12a91d5c4c2f5f91c27947c9
Marshall Petersen <marshall.petersen@gmail.com>
2013-12-07 16:04:29 -0800

    Dpad for controls.

--- sge.js
+++ sge.js
@@ -31088,20 +31088,10 @@ define('sge/components/controls',['sge/component'], function(Component){
             if (this.input===undefined){
                 return;
             }
-            var xaxis = 0;
-            var yaxis = 0;
-            if (this.input.isPressed('down') || this.input.isPressed('S') || this.input.joystick.down()){
-                yaxis++;
-            }
-            if (this.input.isPressed('up') || this.input.isPressed('W') || this.input.joystick.up()){
-                yaxis--;
-            }
-            if (this.input.isPressed('right') || this.input.isPressed('D') || this.input.joystick.right()){
-                xaxis++;
-            }
-            if (this.input.isPressed('left') || this.input.isPressed('A') || this.input.joystick.left()){
-                xaxis--;
-            }
+            var dpad = this.input.dpad();
+            var xaxis = dpad[0];
+            var yaxis = dpad[1];
+
             vx = this.entity.set('xform.vx', xaxis * this.data.speed);
             vy = this.entity.set('xform.vy', yaxis * this.data.speed);
             //this.entity.set('xform.tx', vx * delta, 'add');
